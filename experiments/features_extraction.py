4cd055eb700f3718927b40eb6cf8ea61e395ce68
meghrajeev <mrajeev@cmu.andrew.edu>
2023-12-07 18:42:05 -0500

    Features Updated

--- experiments/features_extraction.py
+++ experiments/features_extraction.py
@@ -9,7 +9,7 @@ sys.path.append('/Users/meghanaarajeev/Documents/ANLP/Subjectivity_With_LLMs')
 from utils_fns import create_predicted_label_leading_q, int_to_str_label, create_acts_labels, get_metrics
 import numpy as np
 client = OpenAI()
-#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
+
 #@meghana: This is code from leading_questions.py I dont personally think you should mimic the "consider all interpreations part", considering we want these to be as objective as possible (since these features are not meant to be subjective). However use your best judgement. Hehe. 
 system_level_prompt = """You will be given question-response pairs from witness testimonials in U.S. congressional hearings. Your job is to pick up on cues of the response which will later be used for determining intent of the response. To aid with this, you will be asked a list of questions. Please answer this as yes/no. """
 
@@ -27,17 +27,16 @@ Questions:
 2. Does the text imply stuttering?
 3. Is the text concise?
 4. Is there repetition between the question and the answer?
-5. Does the text use pronouns like "we" or "us"?
-6. Does the text contain rambling?
-7. Does the text reference external sources or authorities?
-8. Does the text use complicated terms/jargon?
-9. Does the text use sarcasm? 
-10. Does the text contain rhetorical questions?
-11. Does the response directly address and answer the question asked, or does it divert from the topic?
-12. Is the response logically structured and coherent without contradictions?
-13. Does the response use hedging language(e.g. 'it seems', 'possibly', 'might be', 'could be', or similar expressions that indicate uncertainty or non-commitment.)?
-14. Does the prompt use assertive language like (but not limited to) "definitely," "certainly," or "absolutely" to express confidence or conviction?
-15. Does the prompt use evasive language with terms like (but not limited to) to the best of my knowledge" or "as far as I am aware" to avoid direct commitment?
+5. Does the text contain rambling?
+6. Does the text reference external sources or authorities?
+7. Does the text use complicated terms/jargon?
+8. Does the text use sarcasm? 
+9. Does the text contain rhetorical questions?
+10. Does the response directly address and answer the question asked, or does it divert from the topic?
+11. Is the response logically structured and coherent without contradictions?
+12. Does the response use hedging language(e.g. 'it seems', 'possibly', 'might be', 'could be', or similar expressions that indicate uncertainty or non-commitment.)?
+13. Does the prompt use assertive language like (but not limited to) "definitely," "certainly," or "absolutely" to express confidence or conviction?
+14. Does the prompt use evasive language with terms like (but not limited to) to the best of my knowledge" or "as far as I am aware" to avoid direct commitment?
  
 
 Provide answers in the following JSON dict format with yes or no for each question. eg output: 
@@ -46,7 +45,6 @@ Provide answers in the following JSON dict format with yes or no for each questi
  "Stuttering": "yes/no",
  "Concise": "yes/no",
  "Repetition": "yes/no",
- "Pronouns": "yes/no",
  "Rambling": "yes/no",
  "External References": "yes/no",
  "Complicated Terms": "yes/no",
@@ -73,7 +71,7 @@ def parse_response(response):
     return features
 
 if __name__ == '__main__':
-    df = pd.DataFrame(columns=["Index", "Question", "Response", "Labels", "Predicted_Labels", "Features", "Filler Words", "Stuttering", "Concise", "Repetition", "Pronouns", "Rambling", "External References", "Complicated Terms", "Sarcasm", "Rhetorical Questions", "Direct Answer", "Logical Coherence", "Hedging Language", "Assertive Language", "Evasive Language"])
+    df = pd.DataFrame(columns=["Index", "Question", "Response", "Labels", "Predicted_Labels", "Features", "Filler Words", "Stuttering", "Concise", "Repetition", "Rambling", "External References", "Complicated Terms", "Sarcasm", "Rhetorical Questions", "Direct Answer", "Logical Coherence", "Hedging Language", "Assertive Language", "Evasive Language"])
     # TODO: combine train and dev since we are doing 0 shot anyway
     data = pd.read_csv("data/train.tsv", delimiter="\t")
 
